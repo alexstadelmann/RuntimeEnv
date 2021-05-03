76085e79fb88009752b596dae3205e1e7defe1c3
alexstadelmann <alexander.alejandro.alex@gmail.com>
2021-05-03 12:51:53 +0200

    Initial commit

---
+++ Tokenizer.hs
@@ -0,0 +1,46 @@
+module Tokenizer
+( tokenizerZiele
+, tokenizerLTerme
+, tokenizerKlauseln
+) where
+
+
+tokenizerZiele:: String -> [String]
+tokenizerZiele xs = tokenizerZiele' (filter(\a -> a /= '\n' && a /= '\r') xs) [] []
+
+
+--Hilfsfunktion:
+tokenizerZiele' :: String -> String -> [String] -> [String]
+tokenizerZiele' [] wacc lacc = reverse((reverse wacc):lacc)
+tokenizerZiele' (x:xs) wacc lacc
+    | x == ' ' && wacc == "" = tokenizerZiele' xs "" lacc
+    | x == ' ' = error "Lehrzeichen wo es nicht sein darf!"
+    | x == ',' && (head wacc) == ')' = tokenizerZiele' xs "" ((reverse (wacc)):lacc)
+    | otherwise = tokenizerZiele' xs (x:wacc) lacc
+
+
+tokenizerLTerme:: String -> [String]
+tokenizerLTerme xs = tokenizerLTerme' (filter(\a -> a /= '\n' && a /= '\r' && a /= '(' && a /= ')') xs) [] []
+
+
+--Hilfsfunktion:
+tokenizerLTerme' :: String -> String -> [String] -> [String]
+tokenizerLTerme' [] wacc lacc = reverse((reverse wacc):lacc)
+tokenizerLTerme' (x:xs) wacc lacc
+    | x == ' ' && wacc == "" = tokenizerLTerme' xs "" lacc
+    | x == ' ' = error "Lehrzeichen wo es nicht sein darf!"
+    | x == ',' = tokenizerLTerme' xs "" ((reverse (wacc)):lacc)
+    | otherwise = tokenizerLTerme' xs (x:wacc) lacc
+
+
+
+tokenizerKlauseln:: String -> [String]
+tokenizerKlauseln xs = tokenizerKlauseln' (filter(\a -> a /= '\n' && a /= '\r') xs) [] []
+
+
+--Hilfsfunktion:
+tokenizerKlauseln' :: String -> String -> [String] -> [String]
+tokenizerKlauseln' [] wacc lacc = if wacc /= "" then reverse((reverse wacc):lacc) else reverse lacc
+tokenizerKlauseln' (x:xs) wacc lacc
+    | x == '.' = tokenizerKlauseln' xs "" ((reverse (x:wacc)):lacc)
+    | otherwise = tokenizerKlauseln' xs (x:wacc) lacc
