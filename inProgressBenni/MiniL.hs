6efeca7069dc1d3df20b9a31d8e468065e40ac7e
suehling <&ZWzajPrjQ#7>
2021-05-20 11:24:36 +0200

    MiniL um Erstellung der Umgebung env ergänzt

--- inProgressBenni/MiniL.hs
+++ inProgressBenni/MiniL.hs
@@ -8,22 +8,16 @@ data Command = Push String
              | Prompt
                deriving (Show)
 
-{-
-data Mix = Pklausel' PKlausel
-         | Literal' Literal
-         | Int Int
-         | Bool Bool
-         | LTerm' LTerm
-         | String String
-         | Empty deriving (Show)
--}
+data Env = Env{klauseln :: [Int], ziel :: Int, letzte :: Int}
+    deriving (Show)
 
-data Register = Register{inst:: Int, back:: Bool, top:: Int, choice :: Int, ret:: Int, pcounter:: Int}
+data Register = Register{i :: Int, b :: Bool, t :: Int, c :: Int, r :: Int, p :: Int}
     deriving (Show)
 
 type Substitution = [(String, String)]
 
 -- im Folgenden Deklarationen insbesondere für den Tokenizer und für den Parser
+
 data Symbol = Variable String
             | Name String
             | LBracket
@@ -56,6 +50,10 @@ data LTerm = Var String
              deriving (Show, Eq)
 
 
+evaluate :: Programm -> Either String Substitution
+evaluate _ = error "TODO"
+
+
 translate :: Programm -> PCode
 translate (Programm pks z) =
     concat (map translate' pks) ++ translateBody (Just z) ++ [Prompt] where
@@ -81,34 +79,70 @@ translateBody (Just (Ziel ls)) = concat (map translateBody' ls) where
     translateBody'' (NVLTerm s _) = [Push s, Call, Backtrack]
 
 
+createEnv :: PCode -> Env
+createEnv = createEnv' [] 0 0 where
+
+    createEnv' :: [Int] -> Int -> Int -> PCode -> Env
+    createEnv' ks _ c ((Unify _):t) = createEnv' (c:ks) 0 (c+1) t
+    createEnv' ks _ c (Return:(Push s):t) = createEnv' ks (c+1) (c+2) t
+    createEnv' ks z c (Prompt:_) = Env (reverse ks) z c
+    createEnv' ks z c (_:t) = createEnv' ks z (c+1) t
+
+
+c_first :: Env -> Maybe Int
+c_first env = case klauseln env of
+                   [] -> Nothing
+                   ks -> Just $ head ks
+
+
+c_next :: Env -> Int -> Maybe Int
+c_next env = c_next' (klauseln env) where
+
+    c_next' :: [Int] -> Int -> Maybe Int
+    c_next' (h:t) i
+        | h /= i = c_next' t i
+        | otherwise = case t of
+                           (x:_) -> Just x
+                           _ -> Nothing
+
+
+c_goal :: Env -> Int
+c_goal = ziel
+
+
+c_last :: Env -> Int
+c_last = letzte
+
+
+
 
 
 testProgramm :: Programm
 testProgramm = Programm [k1, k2, k3] z3
 
 k1 :: PKlausel
-k1 = PKlausel p (Just z1)
+k1 = PKlausel pt (Just z1)
 
 k2 :: PKlausel
-k2 = PKlausel q (Just z2)
+k2 = PKlausel qt (Just z2)
 
 k3 :: PKlausel
-k3 = PKlausel r Nothing
+k3 = PKlausel rt Nothing
 
 z1 :: Ziel
-z1 = Ziel [Literal False (NVar q)]
+z1 = Ziel [Literal False (NVar qt)]
 
 z2 :: Ziel
-z2 = Ziel [Literal False (NVar r)]
+z2 = Ziel [Literal False (NVar rt)]
 
 z3 :: Ziel
-z3 = Ziel [Literal False (NVar p), Literal False (NVar r)]
+z3 = Ziel [Literal False (NVar pt), Literal False (NVar rt)]
 
-p :: NVLTerm
-p = NVLTerm "p" []
+pt :: NVLTerm
+pt = NVLTerm "p" []
 
-q :: NVLTerm
-q = NVLTerm "q" []
+qt :: NVLTerm
+qt = NVLTerm "q" []
 
-r :: NVLTerm
-r = NVLTerm "r" []
+rt :: NVLTerm
+rt = NVLTerm "r" []
