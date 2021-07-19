c19728728deb328044a3620e9692ce58bba9d7f6
suehling <&ZWzajPrjQ#7>
2021-07-19 16:00:43 +0200

    Code refactoring.

--- Declarations.hs
+++ Declarations.hs
@@ -15,16 +15,14 @@ module Declarations (
     IstNegiert,
     NVLTerm(..),
     LTerm(..),
+    Storage(..)
 ) where
-
 type Stack = [StackElem]
-
 type Result = [Stack]
 
+
 data StackElem = Zahl Int | Atom String deriving (Show, Eq)
-
 data LTermElem = STR String Int | VAR String Int deriving (Show)
-
 type PCode = [Command]
 
 data Env = Env{klauseln :: [Int], goal :: Int, letzte :: Int}
@@ -39,7 +37,7 @@ data Command = Push StackElem
                deriving (Show, Eq)
 
 
-data Register = Register{inst:: Int, backQ:: Bool, top:: Int, choice :: Int, ret:: Int, pcounter:: Int} 
+data Register = Register{i :: Int, b :: Bool, t :: Int, c :: Int, r :: Int, p :: Int}
     deriving (Show)
 
 data Register' = Register' Int Bool Int Int Int Int
@@ -47,32 +45,35 @@ data Register' = Register' Int Bool Int Int Int Int
 
 
 --Deklarationen (insbesondere) für den Tokenizer und für den Parser
-data Symbol = Variable String 
-            | Name String 
-            | LBracket 
-            | RBracket 
-            | Not 
-            | If 
-            | Point 
-            | And 
+data Symbol = Variable String
+            | Name String
+            | LBracket
+            | RBracket
+            | Not
+            | If
+            | Point
+            | And
             deriving (Show)
 
-data Programm = Programm [PKlausel] Ziel 
+data Programm = Programm [PKlausel] Ziel
         deriving (Show)
 
-data PKlausel = PKlausel NVLTerm (Maybe Ziel) 
+data PKlausel = PKlausel NVLTerm (Maybe Ziel)
         deriving (Show)
 
-data Ziel = Ziel [Literal] 
+data Ziel = Ziel [Literal]
         deriving (Show)
 
-data Literal = Literal IstNegiert LTerm 
+data Literal = Literal IstNegiert LTerm
         deriving (Show)
 
 type IstNegiert = Bool
 
-data NVLTerm = NVLTerm String [LTerm] 
+data NVLTerm = NVLTerm String [LTerm]
         deriving (Show, Eq)
 
-data LTerm = Var String | NVar NVLTerm 
+data LTerm = Var String | NVar NVLTerm
         deriving (Show, Eq)
+
+
+type Storage = (Stack, PCode, Env, Register, Result)
--- MiniL/Translator.hs
+++ MiniL/Translator.hs
@@ -5,20 +5,10 @@ module Translator(
    c_next,
    c_goal,
    c_last,
-   -- testProgramm 
+   -- testProgramm
 ) where
 
 import Declarations
-
-linNVLTerm :: NVLTerm -> [LTermElem]
-linNVLTerm (NVLTerm a []) = [STR a 0]
-linNVLTerm (NVLTerm a xs) = (STR a (length xs)): concatMap linLTerm xs
-
-linLTerm :: LTerm-> [LTermElem]
-linLTerm (Var a) = [VAR a (-1)]
-linLTerm (NVar (NVLTerm a [])) = [STR a 0]
-linLTerm (NVar (NVLTerm a xs)) = (STR a (length xs)) : concatMap linLTerm xs
-
 
 
 translate :: Programm -> PCode
@@ -53,13 +43,13 @@ createEnv = createEnv' [] 0 0 where
     createEnv' ks _ c ((Unify _):t) = createEnv' (c:ks) 0 (c+1) t
     createEnv' ks _ c (Return:(Push s):t) = createEnv' ks (c+1) (c+2) t
     createEnv' ks z c (Prompt:_) = Env (reverse ks) z c
-    createEnv' ks z c (_:t) = createEnv' ks z (c+1) t
+    createEnv' ks z c (_:t) = createEnv' ks z (c + 1) t
 
 
 c_first :: Env -> Int
 c_first env = case klauseln env of
-                   [] -> (-1)
-                   ks -> 0
+                   [] -> -1
+                   _ -> 0
 
 
 c_next :: Env -> Int ->  Int
@@ -69,8 +59,8 @@ c_next env = c_next' (klauseln env) where
     c_next' (h:t) i
         | h /= i = c_next' t i
         | otherwise = case t of
-                           (x:_) -> x
-                           _ -> (-1)
+                           x:_ -> x
+                           _ -> -1
 
 
 c_goal :: Env -> Int
@@ -82,35 +72,35 @@ c_last = letzte
 
 
 
--- testProgramm :: Programm
--- testProgramm = Programm [k1, k2, k3] z3
--- 
--- k1 :: PKlausel
--- k1 = PKlausel p (Just z1)
--- 
--- k2 :: PKlausel
--- k2 = PKlausel q (Just z2)
--- 
--- k3 :: PKlausel
--- k3 = PKlausel r Nothing
--- 
--- z1 :: Ziel
--- z1 = Ziel [Literal False (NVar q)]
--- 
--- z2 :: Ziel
--- z2 = Ziel [Literal False (NVar r)]
--- 
--- z3 :: Ziel
--- z3 = Ziel [Literal False (NVar p), Literal False (NVar r)]
--- 
--- z4 :: Ziel
--- z4 = Ziel [Literal False (NVar p)]
--- 
--- p :: NVLTerm
--- p = NVLTerm "p" []
--- 
--- q :: NVLTerm
--- q = NVLTerm "q" []
--- 
--- r :: NVLTerm
--- r = NVLTerm "r" []
+testProgramm :: Programm
+testProgramm = Programm [k1, k2, k3] z3
+
+k1 :: PKlausel
+k1 = PKlausel p' (Just z1)
+
+k2 :: PKlausel
+k2 = PKlausel q' (Just z2)
+
+k3 :: PKlausel
+k3 = PKlausel r' Nothing
+
+z1 :: Ziel
+z1 = Ziel [Literal False (NVar q')]
+
+z2 :: Ziel
+z2 = Ziel [Literal False (NVar r')]
+
+z3 :: Ziel
+z3 = Ziel [Literal False (NVar p'), Literal False (NVar r')]
+
+z4 :: Ziel
+z4 = Ziel [Literal False (NVar p')]
+
+p' :: NVLTerm
+p' = NVLTerm "p" []
+
+q' :: NVLTerm
+q' = NVLTerm "q" []
+
+r' :: NVLTerm
+r' = NVLTerm "r" []
