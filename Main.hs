6daba0be12676d8542ccc82a65ae760fdffbba3f
suehling <&ZWzajPrjQ#7>
2021-07-20 15:58:48 +0200

    Reading a MiniL program from a file is now possible.

--- Main.hs
+++ Main.hs
@@ -8,8 +8,10 @@ import MiniL
 
 main :: IO ()
 main = do
-  inputText <- getLine
-  let tokens = tokenize inputText
+  putStrLn "Please, specify a file to be compiled."
+  inputFilePath <- getLine
+  inputProgram <- readFile inputFilePath
+  let tokens = tokenize inputProgram
       syntaxTree = parse tokens
       outputText = show $ miniL syntaxTree
   putStrLn outputText
